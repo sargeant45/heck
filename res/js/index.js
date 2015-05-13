15a7ef593d26c5e854a21b2bde712a7dbea4b8b3
sargeant45 <ethanarterberry@gmail.com>
2015-05-12 17:19:18 -0700

    Finish the animation, and start work on gameplay

--- res/js/index.js
+++ res/js/index.js
@@ -1,13 +1,9 @@
 function begin() {
-    $(".info").hide();
-    $("#title").hide();
-    $("body").animate({backgroundColor: "black"}, "slow");
-    setTimeout(function () {
-        flicker(2, function () {
-                $("#other").fadeIn("slow");
-                setTimeout(function () {$('body').animate({ backgroundColor: "white" }, 'slow');}, 250);
-        })
-    }, 3000);
+    var scarysounds = new buzz.sound("res/mp3/opener.mp3");
+    scarysounds.bind("ended", function(){$('body').animate({ color: "white" }, 'slow');$("#other").fadeIn("slow");});
+    $(".ascensor").slideUp(1000, function(){setTimeout(function(){scarysounds.play();}, 500)});
+    $(".ascensor").fadeOut(1000);
+    $("body").animate({backgroundColor: "black"}, 1000);
 }
 
 function flicker(count, callback, current) {
@@ -23,12 +19,15 @@ function flicker(count, callback, current) {
             return;
         }
         flicker(count, callback, current + 1)
-    }, 100);
+    }, 300);
 }
 
 function randomQuestion() {
     var typenum = chance.integer({min: 1, max: 3});
     var type;
+    var yesno = ["Can penguins fly?", "Does a pentagram have 5 sides?", "Are blueberries blue?", "If one meatbag is the weight of 2 bags of meat, then what is a lie?", "Does a square have 4 sides?", "On a scale of one to ten, what is real?", "Does 六角星 mean pentagram?", "Is Lord Chin-Chin the best Lord Chin-Chin?", "Is Mario a communist?", "On a scale of one to ten, are you a dick?", "Did aliens build the pyramids?", "On a scale of one to ten, do you hail Satan 666 Bush did 9/11?", "If you have 2 cookies, the Illuminati diamond, a knife, and a pentagram, are sloths badass?"];
+    // Answers, true = yes, false = no
+    var yesnocorrect = [true, false, false, true, false, true, false, true, false, true, false, true, true];
     switch(typenum)
     {
             case 1:
@@ -40,10 +39,5 @@ function randomQuestion() {
             case 3:
                 type = "challenge";
                 break;
-   }
-   switch(type)
-   {
-           case "yesno":
-                
    }
 }
