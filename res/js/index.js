7d9a691e67f156237805518665f83fd8f9bbf231
Ethan Arterberry <ethanarterberry@gmail.com>
2015-05-12 23:57:30 -0700

    Really start to work on the game.

--- res/js/index.js
+++ res/js/index.js
@@ -1,38 +1,14 @@
-function begin() {
-    intro();
-}
-
 function intro() {
     // Do the scary shit lol
     var scarysounds = new buzz.sound("res/mp3/opener.mp3");
     var unnamedmusicstuff = new buzz.sound("res/mp3/moosic.mp3");
-    unnamedmusicstuff.loop = true;
-    scarysounds.bind("ended", function(){$('body').animate({ color: "white" }, 'slow');$("#other").fadeIn("slow");setTimeout(function() {unnamedmusicstuff.play(); /* Rainbows are fucking awesome */ rainbows();}, 2000);});
-    $(".ascensor").slideUp(1000, function(){setTimeout(function(){scarysounds.play();}, 500)});
+    music(true, unnamedmusicstuff);
+    $(".ascensor").slideUp(1000, function(){setTimeout(function(){scarysounds.play();}, 500);});
     $(".ascensor").fadeOut(1000);
     $("body").animate({backgroundColor: "black"}, 1000);
+    // Rainbows!!
+    scarysounds.bind("ended", function(){$('body').animate({ color: "white" }, 'slow');$("#other").fadeIn("slow");setTimeout(function() {music(false, unnamedmusicstuff); /* Rainbows are fucking awesome */ rainbows();}, 2000); setTimeout(function(){game();}, 15179);});
 }
-
-function randomQuestion() {
-    var typenum = chance.integer({min: 1, max: 3});
-    var type;
-    var yesno = ["Can penguins fly?", "Does a pentagram have 5 sides?", "Are blueberries blue?", "If one meatbag is the weight of 2 bags of meat, then what is a lie?", "Does a square have 4 sides?", "On a scale of one to ten, what is real?", "Does 六角星 mean pentagram?", "Is Lord Chin-Chin the best Lord Chin-Chin?", "Is Mario a communist?", "On a scale of one to ten, are you a dick?", "Did aliens build the pyramids?", "On a scale of one to ten, do you hail Satan 666 Bush did 9/11?", "If you have 2 cookies, the Illuminati diamond, a knife, and a pentagram, are sloths badass?"];
-    // Answers, true = yes, false = no
-    var yesnocorrect = [true, false, false, true, false, true, false, true, false, true, false, true, true];
-    switch(typenum)
-    {
-            case 1:
-                type = "yesno";
-                break;
-            case 2:
-                type = "string";
-                break;
-            case 3:
-                type = "challenge";
-                break;
-   }
-}
-
 function rainbows() {
     // Rainbow stuff
     // Shadow
@@ -49,4 +25,96 @@ function rainbows() {
         $("body").css("backgroundColor", chance.color({format: 'hex'}));
     }, 250);
     setTimeout(function(){clearInterval(colorzyayz); $("#heck-h").css("color", "#B85F08"); $("#heck-e").css("color", "#B85F08"); $("#heck-c").css("color", "#B85F08"); $("#heck-k").css("color", "#B85F08"); $("body").css("backgroundColor", "white"); $("body").css("color", "black"); $("#header").css({"-webkit-text-stroke" : "0px black", "text-shadow" : "0px 0px #ff0000;"});}, 12179);
+}
+
+function game() {
+    randomQuestion();
+}
+
+function music(load, musics) {
+  if(load === true)
+  {
+    musics.load();
+  }
+  else
+  {
+    musics.play();
+    setInterval(function(){musics.play();}, 205000);
+  }
+}
+
+function randomQuestion() {
+    var typenum = /*chance.integer({min: 1, max: 2})*/ 1;
+    
+    // Subtext, or stuff that comes up before the question, is generated randomly.
+    // Mad Libs style!
+    
+    var subtext = "";
+    var exclamations = ["Oh no!", "Leaping lizards!", "Holy balls, Batman!", "Holy Satan's buttcrack!", "Oh my demons!", "Holy astronomy, Batman!", "Holy schizophrenia patient!", "Oh noes!", "Well, slap my tits and call me Shirley!", "Oh my god! It's so intense!", "Incarcerated dumptrucks, Batman!", "Great balls of fire!", "This situation is so intense, I'd put it on Shark Tank!", "No way!", "Well piss on my dog and call me Batman, Batman!", "Holy incarceration, Batman!", "Oh my god, Robin!", "I can't even!", "I have lost the ability to even!", "Doz nuts tho, Batman!", "We're in a pickle!", "This situation looks worse than yo momma!"];
+    var person = ["Satan's skeletal army", "Weeaboo Jones", "The developer", "Your mom", "Satan", "Satan's gatekeeper", "A demon", "Your bratty little sister", "Lord Chin-Chin", "The one black kid from Little Einsteins", "Spoderman", "A wither skeleton", "Walter White", "Linus Torvalds", "Brandi Maxxx", "A confederate supporter", "An honorable businessman", "The kid from Limbo", "Foster the People", "One Direction", "Justin Bieber", "Dolan", "Satan's army of gigantic 8-legged pigs", "Filthy Frank", "Smosh", "Shane Dawson", "Robin", "Mickey Mouse", "A dead whale", "A dumbass hipster", "A pseudo-intellectual white kid", "Iggy Azaelea", "Kanye West"];
+    var action = ["has you!", "wants to pop a niner in your face with a deuce deuce!", "will drag you into the depths of Heck, once and for all!", "is following you, in an attempt to kill you with a very certain set of skills.", "will hack into your mental system and use your body to take over Heck and then the world!", "is on the hunt for a you, with three rules: \"Get paid, get laid, and execute the target via Gatorade\".", "will eat you!"];
+    
+    // All questions correlate with an answer because they are at the same index in their arrays, so the randomly picked number will access both of them
+    var question = "";
+    
+      // Yes or no questions
+      var yesno = ["Can penguins fly?", "Does a pentagram have 5 sides?", "Are blueberries blue?", "If one meatbag is the weight of 2 bags of meat, then what is a lie?", "Does a square have 4 sides?", "On a scale of one to ten, what is real?", "Does 六角星 mean pentagram?", "Is Lord Chin-Chin the best Lord Chin-Chin?", "Is Mario a communist?", "On a scale of one to ten, are you a dick?", "Did aliens build the pyramids?", "On a scale of one to ten, do you hail Satan 666 Bush did 9/11?", "If you have 2 cookies, the Illuminati diamond, a knife, and a pentagram, are sloths badass?", "1 hole in yes?"];
+      // Answers, true = yes, false = no
+      var yesnocorrect = [true, false, false, true, false, true, false, true, false, true, false, true, true, true];
+      
+      // Word answer questions (multiple choice)
+      var wordanswer = ["What is blue?", "How many faces on a pentagram?", "How many episodes of Star Wars were there?", "How many Indiana Jones movies were there?", "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"];
+      // Correct answer
+      var wordanswercorrect = ["your face", "1", "3", "3", "42"];
+      // Incorrect answer
+      var wordanswerincorrect = ["blueberries", "5", "6", "4", "It's a groundhog."];
+    
+    switch(typenum)
+    {
+            case 1:
+                // Yes or no.
+                // Format subtext.
+                subtext = exclamations[chance.integer({min: 0, max: exclamations.length})] + " " + person[chance.integer({min: 0, max: person.length})] + " " + action[chance.integer({min: 0, max: exclamations.length})] + " Answer a question if you want any chance of surviving!";
+                alert(subtext);
+                var questionIndex = chance.integer({min: 0, max: yesno.length});
+                question = "Your question is:\n" + yesno[questionIndex] + "\nPlease type 'y' if yes, and type 'n' if no.";
+                var useranswer = prompt(question, "y or n").toLowerCase();
+                var useranswerbool;
+                switch(useranswer)
+                {
+                  case "y":
+                    useranswerbool = true;
+                    break;
+                  case "n":
+                    useranswerbool = false;
+                    break;
+                  default:
+                    alert("You broke a rule of Heck.\nSee you later.");
+                    close();
+                    break;
+                }
+                if(useranswerbool === yesnocorrect[questionIndex])
+                {
+                  correct();
+                }
+                else
+                {
+                  incorrect();
+                }
+                break;
+            case 2:
+                // Word answer.
+                // Format subtext.
+                subtext = exclamations[chance.integer({min: 0, max: exclamations.length})] + " " + person[chance.integer({min: 0, max: person.length})] + " " + action[chance.integer({min: 0, max: exclamations.length})] + " Answer a question if you want any chance of surviving!";
+                alert(subtext);
+                break;
+   }
+}
+
+function correct() {
+  
+}
+
+function incorrect() {
+  // code some game over stuff here, but for later.
 }
